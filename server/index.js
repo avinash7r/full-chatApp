7e6028f9cef34aa5fe3dd6d41b96c2fef027dae7
avinash <rajureavinash7@gmail.com>
2025-04-28 17:34:24 +0530

    added chat-app.avinashrajure.live as url

--- server/index.js
+++ server/index.js
@@ -2,13 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './src/lib/connectDB.js';
-import {router as authRoutes} from './src/routes/auth.routes.js';
-import {router as messageRoutes} from './src/routes/message.routes.js';
+import { router as authRoutes } from './src/routes/auth.routes.js';
+import { router as messageRoutes } from './src/routes/message.routes.js';
 import { app, io, server } from './src/lib/socket.js';
 import cors from 'cors';
 import path from 'path';    
 const __dirname = path.resolve();
 dotenv.config();
+
+app.use((req, res, next) => {
+    const host = req.headers.host;
+    if (host && host.includes('onrender.com')) {
+      return res.redirect(301, `https://chat-app.avinashrajure.live${req.url}`);
+    }
+    
+    next();
+  });
+  
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -16,14 +27,14 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname,"../client/dist")));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../client","dist","index.html"));
-    })
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
 }
 const PORT = process.env.PORT;
 
